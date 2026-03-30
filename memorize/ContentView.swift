fb75a8e4a7a649e6900af8465e04573de88c8d34
yenchunn <a11344245@userdeiMac-2.local>
2026-03-30 17:27:20 +0800

    feat: add LazyVCard

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,32 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["A", "B" , "C" , "D" , "E" , "F","G","H","I","J","K","L","M","N","O"]
+    
+    @State var emojiCount = 6
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            cardList
+            Spacer()
+            actionButtons
+            
+                .font(.largeTitle)
         }
         .padding()
         .foregroundStyle(.orange)
     }
-}
-
-struct CardView: View {
-    var isFaceUp: Bool = true
-    var body: some View{
-        ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 20)
-                    .strokeBorder(lineWidth: 3)
-                    .foregroundStyle(.orange)
-            Text("✈️")
+    
+    var cardList: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0){
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                    
+                }
             }
+        }
+    }
+    
+    var actionButtons: some View {
+        HStack {
+            remove
+            Spacer()
+            add
+        }
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1{
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View{
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        }label: {
+            Image(systemName: "plus.circle")
+        }
+        
+    }
+}
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content:String
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            Group {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 3)
                 
+                Text(content)
+                    .font(Font.system(size: 300))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1,contentMode: .fit)
+            }
+
+                .opacity(isFaceUp ? 1 : 0)
+                
+                shape.opacity(isFaceUp ? 0 : 1)
+        }
+        
+        
+        .onTapGesture {
+            isFaceUp = !isFaceUp
         }
     }
 }
+
 #Preview {
     ContentView()
 }
